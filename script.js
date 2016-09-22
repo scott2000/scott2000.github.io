652bbde318c523d979a4cbe54a804ac0156d61e2
scott2000 <scottipod2000@gmail.com>
2016-09-22 16:21:49 -0500

    Added Kinematic Equation Solver

--- script.js
+++ script.js
@@ -63,6 +63,61 @@ function listFactors(number) {
     }
 }
 
+function a(vo, v, x, t) {
+    if (v === '') {
+        return ((2*x) - (2*vo*t))/(t*t);
+    } else if (x === '') {
+        return (v-vo)/t;
+    } else if (t === '') {
+        return ((v*v)-(vo*vo))/(2*d);
+    }
+    return '?';
+}
+
+function vo(a, v, x, t) {
+    if (a === '') {
+        return ((2*x)/t)-v;
+    } else if (x === '') {
+        return vo-(a*t);
+    } else if (t === '') {
+        return Math.sqrt((v*v)-(2*a*x));
+    }
+    return '?';
+}
+
+function v(a, vo, x, t) {
+    if (a === '') {
+        return ((2*x)/t)-vo;
+    } else if (x === '') {
+        return (a*t)+vo;
+    } else if (t === '') {
+        return Math.sqrt((vo*vo)+(2*a*x));
+    }
+    return '?';
+}
+
+function x(a, vo, v, t) {
+    if (a === '') {
+        return t*(vo+v)/2;
+    } else if (v === '') {
+        return (vo*t)+((a*t*t)/2);
+    } else if (t === '') {
+        return ((v*v)-(vo*vo))/(2*a);
+    }
+    return '?';
+}
+
+function t(a, vo, v, x) {
+    if (a === '') {
+        return (2*x)/(vo+v);
+    } else if (v === '') {
+        return 2*(x-(vo*t))/a;
+    } else if (x === '') {
+        return (v-vo)/a;
+    }
+    return '?';
+}
+
 $(document).ready(function() {
     var curr = new Date().getTime()/1000 | 0;
     var last = curr-parseInt(getCookie('last'));
@@ -192,5 +247,34 @@ $(document).ready(function() {
             $('input[name=b]').val('');
             $('input[name=c]').val('');
         }
+    });
+
+    //Kinematic Equation Submit
+    $('#submitKinematic').click(function() {
+        var acceleration = $('input[name=acceleration]').val();
+        var initialVelocity = $('input[name=initialVelocity]').val();
+        var velocity = $('input[name=velocity]').val();
+        var position = $('input[name=position]').val();
+        var time = $('input[name=time]').val();
+        if (acceleration === '') {
+            $('input[name=acceleration]').val(a(initialVelocity, velocity, position, time));
+        }
+        if (initialVelocity === '') {
+            $('input[name=acceleration]').val(vo(acceleration, velocity, position, time));
+        }
+        if (velocity === '') {
+            $('input[name=acceleration]').val(v(acceleration, initialVelocity, position, time));
+        }
+        if (position === '') {
+            $('input[name=acceleration]').val(x(acceleration, initialVelocity, velocity, time));
+        }
+        if (time === '') {
+            $('input[name=acceleration]').val(t(acceleration, initialVelocity, velocity, position));
+        }
+    });
+
+    //Kinematic Equation Clear
+    $('#clearKinematic').click(function() {
+        $('input[class=kinematicBox]').val('');
     });
 });
